b0bdd9704c6013eb5ebc8318b5ca4135e7849a2c
Just1nBon1n <e2263742@cmaisonneuve.qc.ca>
2025-12-10 10:55:17 -0500

    Test son spatial

---
+++ Assets/Scripts/SonsScipt/SonsManagerJeu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SonsManagerJeu : MonoBehaviour
+{
+    public AudioSource musiqueDeFond;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        musiqueDeFond.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SonsScipt/SonsManagerLobby.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ManagerMusiqueLobbyFin : MonoBehaviour
+{
+    public AudioSource audioSourceLobbyFin;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSourceLobbyFin.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
